f70e10189cee4a2603c06aa8c1c2951891f2e9e8
Siowyn <siowyn@siowyn.com>
2023-10-22 20:57:10 +0300

    fixes

--- src/todolist.js
+++ src/todolist.js
@@ -1,29 +1,36 @@
 import React, { useEffect, useState } from "react";
-import Button from "./button";
+import Buttona from "./button";
 import {
-  Buttona,
+  Button,
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter,
 } from "reactstrap";
-import CreateTask from "./modal.js";
 function TodoList() {
   const [modal, setModal] = useState(false);
 
   const toggle = () => setModal(!modal);
 
   return (
-    <div className="modalcontainer">
-      <Button color="danger" onClick={toggle}>Click Me</Button>
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+    <div className="modaldiv">
+      <Buttona color="danger" onClick={toggle}>Click Me</Buttona>
+      <Modal isOpen={modal} toggle={toggle} className="modalcontainer">
+        <ModalHeader className="modalheader"toggle={toggle}>TODO ADD</ModalHeader>
         <ModalBody className="modalcss">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          <form>
+            <div className="form-group">
+                <input type="text" className="form-control" placeholder="Task Name"></input>
+            </div>
+            <br></br>
+            <div className="form-group">
+                <textarea rows="4" className="form-control" placeholder="Task Description"></textarea>
+              </div>
+          </form>
         </ModalBody>
-        <ModalFooter>
+        <ModalFooter className="modalfooter">
           <Button color="primary" onClick={toggle} className="modalbutton">
-            Do Something
+           Create
           </Button>{' '}
           <Button color="secondary" onClick={toggle} className="modalbutton">
             Cancel
