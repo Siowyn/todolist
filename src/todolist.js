52723544b4dbdd5eb8765a56e5e335a7c5b27a6f
Latif <latif.hensbergen@icloud.com>
2023-10-23 21:04:13 +0200

    feat: instead of force reloading the page it now triggers a rerender

--- src/todolist.js
+++ src/todolist.js
@@ -1,79 +1,87 @@
-import React, {useEffect, useState} from "react";
-import Card from "./cards.js"
+import React, { useEffect, useState } from "react";
+import Card from "./cards.js";
 function TodoList() {
-  const [modal, setModal] = useState(false);
-  const [taskName, setTaskName] = useState('');
-  const [description, setDescription] = useState('');
-
-
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
   const [taskList, setTaskList] = useState([]);
 
   const handleOnChange = (e) => {
-    const  {name, value} = e.target
-    if (name === "taskName")
-    {
-      setTaskName(value)
-    }else
-    {
-      setDescription(value)
+    const { name, value } = e.target;
+    if (name === "taskName") {
+      setTaskName(value);
+    } else {
+      setDescription(value);
     }
-  }
+  };
   useEffect(() => {
-    let arr = localStorage.getItem("taskList")
-    if (arr)
-    {
-      let obj = JSON.parse(arr)
-      setTaskList(obj)
+    let arr = localStorage.getItem("taskList");
+    if (arr) {
+      let obj = JSON.parse(arr);
+      setTaskList(obj);
     }
-  }, [])
+  }, [taskList]);
 
   const saveTask = (taskObject) => {
-    let tempList = taskList
-    tempList.push(taskObject)
-    localStorage.setItem("taskList", JSON.stringify(tempList))
-    setTaskList(tempList)
-    window.location.reload();
-  }
+    let tempList = taskList;
+    tempList.push(taskObject);
+    localStorage.setItem("taskList", JSON.stringify(tempList));
+    setTaskList(tempList);
+  };
 
   const handleSave = () => {
-    if(taskName && description)
-    {
-      let taskObject = {}
-      taskObject["Name"] = taskName
-      taskObject["Description"] = description
-      saveTask(taskObject)
-    }else
-    {
-      alert("Task Name & Description Cannot Be Empty")
+    if (taskName && description) {
+      let taskObject = {};
+      taskObject["Name"] = taskName;
+      taskObject["Description"] = description;
+      saveTask(taskObject);
+    } else {
+      alert("Task Name & Description Cannot Be Empty");
     }
-  }
+  };
 
-  const deleteTask = (index) =>{
-    let tempList = taskList
-    tempList.splice(index, 1)
-    localStorage.setItem("taskList", JSON.stringify(tempList))
-    setTaskList(tempList)
-    window.location.reload();
-
-  }
+  const deleteTask = (index) => {
+    let tempList = taskList;
+    tempList.splice(index, 1);
+    localStorage.setItem("taskList", JSON.stringify(tempList));
+    setTaskList(tempList);
+  };
 
   return (
     <>
       <div className="modaldiv">
-      <form>
-            <div className="form-group">
-                <input name="taskName" type="text" className="form-control" placeholder="Task Name" value={taskName} onChange={handleOnChange}></input>
-            </div>
-            <br></br>
-            <div className="form-group">
-                <input name="description"rows="4" className="form-control" placeholder="Task Description" value={description}  onChange={handleOnChange}></input>
-              </div>
-          </form>
-		            <button onClick={handleSave} className="modalbutton">Add Task</button>
+        <form>
+          <div className="form-group">
+            <input
+              name="taskName"
+              type="text"
+              className="form-control"
+              placeholder="Task Name"
+              value={taskName}
+              onChange={handleOnChange}
+            ></input>
+          </div>
+          <br></br>
+          <div className="form-group">
+            <input
+              name="description"
+              rows="4"
+              className="form-control"
+              placeholder="Task Description"
+              value={description}
+              onChange={handleOnChange}
+            ></input>
+          </div>
+        </form>
+        <button onClick={handleSave} className="modalbutton">
+          Add Task
+        </button>
       </div>
-    <div className="taskContainer">
-       {taskList && taskList.map((obj, index) => <Card taskObject = {obj} index={index} deleteTask = {deleteTask}/>)}
-    </div>
+      <div className="taskContainer">
+        {taskList &&
+          taskList.map((obj, index) => (
+            <Card taskObject={obj} index={index} deleteTask={deleteTask} />
+          ))}
+      </div>
     </>
   );
 }
